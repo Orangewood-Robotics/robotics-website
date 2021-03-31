642034e826eb555541f9eef8663e6b01537dea7d
eddie280 <40438479+eddie280@users.noreply.github.com>
2021-03-31 09:35:27 -0700

    Deleted old JS comment

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function(){stickyNav()};
-
-var navbar = document.getElementById("nav--links");
-
-var sticky = navbar.offsetTop;
-
-function stickyNav(){
-    window.pageYOffset >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}; 
