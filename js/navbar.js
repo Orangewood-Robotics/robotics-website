df8689230ee013a0b1f32fb88600df6b09b2e659
eddie280 <40438479+eddie280@users.noreply.github.com>
2021-03-30 18:03:58 -0700

    Finished the core of the navbar. Testing out the sticky navbar function with JS. Still need to fix. Will come back later tonight.

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,9 @@
-console.log('Hello world')
+window.onscroll = function(){stickyNav()};
+
+var navbar = document.getElementById("nav--links");
+
+var sticky = navbar.offsetTop;
+
+function stickyNav(){
+    window.pageYOffset >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}; 
